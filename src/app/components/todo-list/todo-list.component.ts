6ba60f114b9b01657742cff471e4aa778258613b
Stephen Ondieki <stevehotcodes@gmail.com>
2024-09-03 16:55:58 +0300

    create todo-app with cypress

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,107 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { TodoItem } from '../../interfaces/types';
+import { CommonModule, NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [NgForOf,FormsModule,CommonModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent implements OnInit {
+  @ViewChild('todoText') todoInputRef:ElementRef<HTMLInputElement> =null!;
+
+  todoList:TodoItem[]=[];
+  editMode: boolean = false;
+  editTaskId?: number | string;
+  newTaskText: string = '';
+  completedList:TodoItem[]=[]
+  incompleted:TodoItem[]=[]
+
+  constructor(){}
+   
+  ngOnInit(): void {
+
+   this.loadTasks();
+   
+  }
+
+  loadTasks(){
+    const storedTodoList=localStorage.getItem('todoList');
+    storedTodoList?this.todoList= JSON.parse(storedTodoList)  : null
+  }
+
+  fetchCompletedTasks(){
+    console.log("complted method")
+    this.completedList = this.todoList.filter(task => task.completed)
+
+  }
+
+  saveTodoItem(){
+    localStorage.setItem('todoList',JSON.stringify(this.todoList))
+}
+
+
+
+  addTask(text:string):void{
+    console.log('add task')
+    if(text.trim()!==''){
+      const newTodoItem:TodoItem={
+        id:Date.now(),
+        task:text.trim(),
+        completed:false
+      }
+      this.todoList.push(newTodoItem);
+      this.todoInputRef.nativeElement.value = '';
+      this.saveTodoItem()
+
+    }
+  }
+
+  deleteTask(id:string|number){
+    this.todoList=this.todoList.filter((item)=>item.id!==id);
+    this.saveTodoItem()
+
+  }
+
+  editTask(id: string|number): void {
+    this.editMode = true;
+    this.editTaskId = id;
+    const taskToEdit = this.todoList.find(task => task.id === id);
+    if (taskToEdit) {
+      this.newTaskText = taskToEdit.task;
+    }
+  }
+
+  updateTask(): void {
+    if (this.editTaskId !== undefined && this.newTaskText.trim()) {
+      const taskIndex = this.todoList.findIndex(task => task.id === this.editTaskId);
+      if (taskIndex !== -1) {
+        this.todoList[taskIndex].task = this.newTaskText;
+        this.saveTodoItem();
+        this.editMode = false;
+        this.newTaskText = '';
+      }
+    }
+  }
+  cancelEdit(): void {
+    this.editMode = false;
+    this.newTaskText = '';
+  }
+
+  toggleCompleted(id:string |number){
+    const todoItem=this.todoList.find(item=>item.id===id)
+    if(todoItem){
+        todoItem.completed=!todoItem.completed
+        this.saveTodoItem();
+        this.loadTasks();
+        this.fetchCompletedTasks()
+        
+    }
+  }
+
+ 
+
+}
---
+++ src/app/interfaces/types.ts
@@ -0,0 +1,5 @@
+export interface TodoItem{
+    id:string | number,
+    task:string,
+    completed:boolean
+}
